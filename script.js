c8ce79352850796251aa026f261231dedd5433d0
UP940298 <72414905+UP940298@users.noreply.github.com>
2022-02-21 16:19:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
-var jsonRaw = {
-  obj1: { 
+var jsonRaw = [
+  {
     "ID": "1",
     "Title": "Good News",
     "Author": "Thomas Percival",
     "Topic": "Good",
     "Date": "21/02/2022",
         },
-   obj2: { 
+    { 
     "ID": "2",
     "Title": "Good News",
     "Author": "Thomas Percival",
     "Topic": "Good",
      "Date": "21/02/2022",
         },
-   obj3: { 
+    { 
     "ID": "3",
     "Title": "Good News",
     "Author": "Marina Bostan",
     "Topic": "Good",
      "Date": "21/02/2022",
         },
-   obj4: { 
+  { 
     "ID": "4",
     "Title": "Amazing",
     "Author": "Marina Bostan",
     "Topic": "Good",
      "Date": "21/02/2022",
         },
-   obj5: { 
+    { 
     "ID": "5",
     "Title": "Fantastic",
     "Author": "Gizmo Percival",
     "Topic": "Good",
      "Date": "21/02/2022",
         },
-   obj6: { 
+     { 
     "ID": "6",
     "Title": "Wow",
     "Author": "Gizmo Percival",
     "Topic": "Good",
      "Date": "21/02/2022",
-        },
-};
+     }
+];
 
 var jsonStr = JSON.stringify(jsonRaw);
 
