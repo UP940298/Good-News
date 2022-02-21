79184b12204e626d4d07a089ceabd4d94afbf3d6
UP940298 <72414905+UP940298@users.noreply.github.com>
2022-02-21 16:56:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,46 @@
-var jsonRaw = [
-  {
-    "ID": "1",
-    "Title": "Good News",
-    "Author": "Thomas Percival",
-    "Body": "Lorus Epsum",
-    "Topic": "Good",
-    "Date": "21/02/2022",
-        },
-    { 
-    "ID": "2",
-    "Title": "Good News",
-    "Author": "Thomas Percival",
-    "Body": "Lorus Epsum",
-    "Topic": "Good",
-     "Date": "21/02/2022",
-        },
-    { 
-    "ID": "3",
-    "Title": "Good News",
-    "Author": "Marina Bostan",
-    "Body": "Lorus Epsum",
-    "Topic": "Good",
-     "Date": "21/02/2022",
-        },
-  { 
-    "ID": "4",
-    "Title": "Amazing",
-    "Author": "Marina Bostan",
-    "Body": "Lorus Epsum",
-    "Topic": "Good",
-     "Date": "21/02/2022",
-        },
-    { 
-    "ID": "5",
-    "Title": "Fantastic",
-    "Author": "Gizmo Percival",
-    "Body": "Lorus Epsum",
-    "Topic": "Good",
-     "Date": "21/02/2022",
-        },
-     { 
-    "ID": "6",
-    "Title": "Wow",
-    "Author": "Gizmo Percival",
-    "Body": "Lorus Epsum",
-    "Topic": "Good",
-     "Date": "21/02/2022",
-     }
-];
+var jsonRaw = [{
+	"ID": "1",
+	"Title": "Good News",
+	"Author": "Thomas Percival",
+	"Body": "Lorus Epsum",
+	"Topic": "Good",
+	"Date": "21/02/2022"
+}, {
+	"ID": "2",
+	"Title": "Good News",
+	"Author": "Thomas Percival",
+	"Body": "Lorus Epsum",
+	"Topic": "Good",
+	"Date": "21/02/2022"
+}, {
+	"ID": "3",
+	"Title": "Good News",
+	"Author": "Marina Bostan",
+	"Body": "Lorus Epsum",
+	"Topic": "Good",
+	"Date": "21/02/2022"
+}, {
+	"ID": "4",
+	"Title": "Amazing",
+	"Author": "Marina Bostan",
+	"Body": "Lorus Epsum",
+	"Topic": "Good",
+	"Date": "21/02/2022"
+}, {
+	"ID": "5",
+	"Title": "Fantastic",
+	"Author": "Gizmo Percival",
+	"Body": "Lorus Epsum",
+	"Topic": "Good",
+	"Date": "21/02/2022"
+}, {
+	"ID": "6",
+	"Title": "Wow",
+	"Author": "Gizmo Percival",
+	"Body": "Lorus Epsum",
+	"Topic": "Good",
+	"Date": "21/02/2022"
+}];
 
 var jsonStr = JSON.stringify(jsonRaw);
 
