88bdbda0620d935d92059fc41f60931aa9b65f97
UP940298 <UP940298@myport.ac.uk>
2022-03-01 16:32:26 +0000

    Firebase added to app for database.

--- Good News/ContentView.swift
+++ Good News/ContentView.swift
@@ -1,37 +1,63 @@
 import SwiftUI
+import Firebase
+import FirebaseAnalytics
+import FirebaseFirestore
 
-struct Article: Codable {
+struct Article: Identifiable {
     var id: String
     var Title: String
-    var Topic: String
+    var Author: String
+    var ArType: String
     var Body: String
     var Date: String
     var Image: String
     var Color: String
 }
 
+class ArticleViewModel: ObservableObject{
+    @Published var articles = [Article]()
+    
+    private var db = Firestore.firestore()
+    
+    func fetchData() {
+        
+        //db.collection("articles").addDocument(data: ["Title": "Good news", "Author": "Thomas Percival", "Body": "", "ArType": "article", "Date":"2022-02-28", "Image":"", "Color":"background1"])
+
+        db.collection("articles").addSnapshotListener { (querySnapshot, error) in guard let documents = querySnapshot?.documents else {
+                print("nah")
+                return
+            }
+
+            self.articles = documents.map{ queryDocumentSnapshot -> Article in
+                let data = queryDocumentSnapshot.data()
+                let Title = data["Title"] as? String ?? ""
+                let Author = data["Author"] as? String ?? ""
+                let Body = data["Body"] as? String ?? ""
+                let ArType = data["Type"] as? String ?? ""
+                let Date = data["Date"] as? String ?? ""
+                let Image = data["Image"] as? String ?? ""
+                let Color = data["Color"] as? String ?? ""
+
+                return Article(id: .init(), Title: Title, Author: Author, ArType: ArType, Body: Body, Date: Date, Image: Image, Color: Color)
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     
-    @State var articles: [Article] = []
-    
+    @ObservedObject var viewModel = ArticleViewModel()
+        
     @State var showContent = false
         
     var body: some View {
-        ScrollView {
-           VStack {
-              HStack {
-                 VStack(alignment: .leading) {
-                    Text("Here's some good")
-                       .font(.largeTitle)
-                       .fontWeight(.heavy)
-                 }
-                 Spacer()
-              }
-              .padding(.leading, 60.0)
-
-              ScrollView(.horizontal, showsIndicators: false) {
-                 VStack(spacing: 40.0) {
-                     ForEach(articles, id: \.Title) { (post) in
+        VStack {
+          Text("Here's some good")
+             .font(.largeTitle)
+             .fontWeight(.heavy)
+            ScrollView(.vertical, showsIndicators: false) {
+                 VStack(spacing: 30.0) {
+                     ForEach(viewModel.articles, id: \.Title) { (post) in
                        Button(action: { self.showContent.toggle() }) {
                           GeometryReader { geometry in
                               ArticleView(title: post.Title,
@@ -41,40 +67,19 @@ struct ContentView: View {
                                    Double(geometry.frame(in: .global).minX - 30) / -40), axis: (x: 0, y: 10.0, z: 0))
                                 //.sheet(isPresented: self.$showContent) {}
                           }
-                          .frame(width: 355, height: 200)
-                          .offset(x: -7, y:0)
+                          .frame(width: 400, height: 20)
+                          .offset(x: -10, y:0)
                        }
                     }
                  }
-                 .padding(.leading, 30)
-                 .padding(.top, 30)
+                 .padding(.leading, 45)
+                 .padding(.top, 20)
                  .padding(.bottom, 70)
                  Spacer()
               }
-           }
-           .padding(.top, 0)
-           .onAppear(perform: {
-               guard let url: URL = URL(string: "http://localhost/dataFetch.php") else {
-                   print("URL is invalid or server is not running.")
-                   return
-               }
-               var urlRequest: URLRequest = URLRequest(url: url)
-               urlRequest.httpMethod = "GET"
-               URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
-                   // Check if response is okay
-                   guard let data = data else {
-                       print("invalid response")
-                       return
-                   }
-                   // Convert JSON response into class model as an array
-                   do {
-                       self.articles = try JSONDecoder().decode([Article].self, from: data)
-                       print(articles)
-                   } catch {
-                       print(String(describing: error))
-                   }
-               }).resume()
-           })
+              .onAppear(perform: {
+               self.viewModel.fetchData()
+        })
         }
     }
 }
@@ -88,24 +93,16 @@ struct ArticleView: View {
    var body: some View {
       return VStack(alignment: .leading) {
          Text(title)
-            .font(.title2)
-            .fontWeight(.bold)
+              .font(Font.custom("Arial", size: 16))
             .foregroundColor(.white)
-            .padding(20)
-            .lineLimit(4)
-
+            .padding(10)
          Spacer()
-
             Image(image)
-              .resizable()
-              .renderingMode(.original)
-              .aspectRatio(contentMode: .fit)
-              .frame(width: 370, height: 100)
-              .padding(.bottom, 30)
+              .frame(width: 400, height: 0)
       }
       .background(Color(color))
-      .cornerRadius(40)
-      .frame(width: 370, height: 200)
+      .cornerRadius(10)
+      .frame(width: 375, height: 20)
    }
 }
 
--- Good News/Good_NewsApp.swift
+++ Good News/Good_NewsApp.swift
@@ -1,7 +1,18 @@
 import SwiftUI
+import Firebase
+
+class MyAppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ app: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct Good_NewsApp: App {
+    
+    @UIApplicationDelegateAdaptor(MyAppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
